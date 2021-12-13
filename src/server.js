0bc1195b50785d8d3c5404e4010d02fbed1f66d4
Lucas Davi Ferreira de Faria <lucasfaria@Air-de-Lucas.mshome.net>
2021-12-13 18:57:00 -0300

    Adicionado nova lib e novos testes

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const app = require('express')
 const http = require('http').createServer(app)
 
 const asteriskManager = require('asterisk-manager')
-let ami = asteriskManager(5038, '127.0.0.1', 'admin', 'admin')
+let ami = asteriskManager(5038, '192.168.50.129', 'scudi', 'dev@scudi#2021')
 
-
+ami.on('hangup', function(evt) {
+    
+});
 //aqui altera a função especifica que deseja apenas para testar
 ami.on("peerStatus", async(evt) => {
     console.log(JSON.stringify(evt))
